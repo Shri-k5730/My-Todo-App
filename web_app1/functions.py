6ef40694f08aa7396632da27701efc102c182965
Shri-k5730 <shri.k5730@gmail.com>
2023-12-22 16:32:18 +0530

    Update file path

--- web_app1/functions.py
+++ web_app1/functions.py
@@ -1,8 +1,9 @@
 def get_todos(filepath="D:/pythonProject/web_app1/todos.txt"):
     """ Read text file and return the todos in the file """
-    with open(filepath,'r') as file_local:
+    with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
+
 
 def write_todos(todos_arg, filepath="D:/pythonProject/web_app1/todos.txt"):
     """ Writes todos to the file"""
